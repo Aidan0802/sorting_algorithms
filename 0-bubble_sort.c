c3a2bc80f50ec419848d5d9eb8e2887004aedd5a
aidan <aidanheydenrycht@gmail.com>
2023-10-24 11:12:01 +0200

    [Fix] betty error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,9 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, x, temp;
-	int swap;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swap = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,9 +21,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				swap = 1;
-			}
-			if (swap == 1)
-			{
+
 				for (x = 0; x < size; x++)
 				{
 					if (array[x] == array[size - 1])
@@ -33,9 +29,8 @@ void bubble_sort(int *array, size_t size)
 					else
 						printf("%d, ", array[x]);
 				}
-				printf("\n");	
+				printf("\n");
 			}
-			swap = 0;
 		}
 	}
 }
