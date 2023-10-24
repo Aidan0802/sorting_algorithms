86c8745693e6a08aea5893d3f9eed8076b5bee24
aidan <aidanheydenrycht@gmail.com>
2023-10-24 11:04:36 +0200

    [Fix] output fail

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,17 +3,18 @@
 /**
  * bubble_sort - Sorting a list
  * @array: Interger array to be sorted
- * size: Size of the array
+ * @size: Size of the array
  * Return: void
  */
 
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, x, temp;
+	int swap;
 
 	for (i = 0; i < size - 1; i++)
 	{
-
+		swap = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -21,7 +22,10 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				
+				swap = 1;
+			}
+			if (swap == 1)
+			{
 				for (x = 0; x < size; x++)
 				{
 					if (array[x] == array[size - 1])
@@ -29,8 +33,9 @@ void bubble_sort(int *array, size_t size)
 					else
 						printf("%d, ", array[x]);
 				}
-				printf("\n");
+				printf("\n");	
 			}
+			swap = 0;
 		}
 	}
 }
